3e16a68cfb9dbd8282ac0194dceaef6df47d559a
Roy Hotrabhvanon <sukrith@gmail.com>
2013-03-08 11:09:22 +0000

    version 1.2

---
+++ src/xmlparse/UI.java
@@ -0,0 +1,25 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package xmlparse;
+
+import java.awt.FlowLayout;
+import javax.swing.JFrame;
+import javax.swing.JLabel;
+import javax.swing.JProgressBar;
+
+/**
+ *
+ * @author Roy
+ */
+public class UI extends JFrame {
+    
+    //JFRAME 
+        static JFrame frm = new JFrame();
+        static JProgressBar current = new JProgressBar(0, 100);
+        static JLabel label = new JLabel();
+        
+       
+   
+}
